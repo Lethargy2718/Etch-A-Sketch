9b684ef99132492981979d2217b96fd43862f604
Lethargy2718 <youssefsherief590@gmail.com>
2024-11-09 16:43:51 +0200

    Better design. Fixed sizes.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const sketch = document.querySelector('.sketch')
 let mouseDown = false
 
+// Disable dragging as it caused some glitches.
+document.addEventListener('dragstart', (e) => e.preventDefault());
+document.addEventListener('drop', (e) => e.preventDefault());
 
 // Coloring toggle
 document.addEventListener('mousedown', () => {
     mouseDown = true
-    
 })
 
 document.addEventListener('mouseup', (e) => {
@@ -15,7 +17,7 @@ document.addEventListener('mouseup', (e) => {
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const pixel = document.createElement('div')
-        pixel.style.cssText = "width: 6.25%; height: 6.25%; background-color: white; border: 2px solid rgba(128, 128, 128, 0.5);"
+        pixel.style.cssText = "width: 6.25%; height: 6.25%; background-color: white; border: 1px solid #4A5D6D;"
         sketch.appendChild(pixel)
         
         pixel.addEventListener('mouseover', () => {
